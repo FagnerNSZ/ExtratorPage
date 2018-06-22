a2677d58521f6246d3f539e967257dd2f25c6d91
fgnTeste <fagner.soares@nexa.com.br>
2018-06-22 18:24:54 -0300

    comentários das alterações

--- ExtraindoDadosWeb/Form1.Designer.vb
+++ ExtraindoDadosWeb/Form1.Designer.vb
@@ -33,14 +33,16 @@ Partial Class Form1
         Me.btnLinks = New System.Windows.Forms.Button()
         Me.txtLinks = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBoxProxy = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBoxPass = New System.Windows.Forms.TextBox()
+        Me.TextBoxPorta = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.Url = New System.Windows.Forms.Label()
+        Me.TextBoxUrl = New System.Windows.Forms.TextBox()
         CType(Me.dgvDados, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -48,7 +50,7 @@ Partial Class Form1
         '
         'btnObterPagina
         '
-        Me.btnObterPagina.Location = New System.Drawing.Point(14, 269)
+        Me.btnObterPagina.Location = New System.Drawing.Point(14, 216)
         Me.btnObterPagina.Name = "btnObterPagina"
         Me.btnObterPagina.Size = New System.Drawing.Size(105, 23)
         Me.btnObterPagina.TabIndex = 0
@@ -61,32 +63,30 @@ Partial Class Form1
         Me.txtDados.Multiline = True
         Me.txtDados.Name = "txtDados"
         Me.txtDados.ScrollBars = System.Windows.Forms.ScrollBars.Both
-        Me.txtDados.Size = New System.Drawing.Size(601, 252)
+        Me.txtDados.Size = New System.Drawing.Size(601, 186)
         Me.txtDados.TabIndex = 1
         '
         'txtUrl
         '
         Me.txtUrl.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
         Me.txtUrl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.txtUrl.Location = New System.Drawing.Point(138, 271)
+        Me.txtUrl.Location = New System.Drawing.Point(138, 218)
         Me.txtUrl.Name = "txtUrl"
         Me.txtUrl.Size = New System.Drawing.Size(476, 20)
         Me.txtUrl.TabIndex = 2
-        Me.txtUrl.Text = "http://brvix5valedb020.valenet.valeglobal.net:8023/bi_prd/modulos/validate/defaul" &
-    "t_remedy.asp"
         '
         'dgvDados
         '
         Me.dgvDados.BackgroundColor = System.Drawing.SystemColors.ScrollBar
         Me.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvDados.Location = New System.Drawing.Point(13, 320)
+        Me.dgvDados.Location = New System.Drawing.Point(13, 267)
         Me.dgvDados.Name = "dgvDados"
-        Me.dgvDados.Size = New System.Drawing.Size(601, 138)
+        Me.dgvDados.Size = New System.Drawing.Size(601, 85)
         Me.dgvDados.TabIndex = 3
         '
         'btnImagem1
         '
-        Me.btnImagem1.Location = New System.Drawing.Point(12, 465)
+        Me.btnImagem1.Location = New System.Drawing.Point(12, 368)
         Me.btnImagem1.Name = "btnImagem1"
         Me.btnImagem1.Size = New System.Drawing.Size(98, 23)
         Me.btnImagem1.TabIndex = 4
@@ -96,7 +96,7 @@ Partial Class Form1
         'PictureBox1
         '
         Me.PictureBox1.BackColor = System.Drawing.Color.White
-        Me.PictureBox1.Location = New System.Drawing.Point(116, 465)
+        Me.PictureBox1.Location = New System.Drawing.Point(116, 368)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(498, 37)
         Me.PictureBox1.TabIndex = 5
@@ -104,7 +104,7 @@ Partial Class Form1
         '
         'btnImagem2
         '
-        Me.btnImagem2.Location = New System.Drawing.Point(14, 508)
+        Me.btnImagem2.Location = New System.Drawing.Point(14, 411)
         Me.btnImagem2.Name = "btnImagem2"
         Me.btnImagem2.Size = New System.Drawing.Size(97, 23)
         Me.btnImagem2.TabIndex = 6
@@ -114,7 +114,7 @@ Partial Class Form1
         'PictureBox2
         '
         Me.PictureBox2.BackColor = System.Drawing.Color.White
-        Me.PictureBox2.Location = New System.Drawing.Point(116, 508)
+        Me.PictureBox2.Location = New System.Drawing.Point(116, 411)
         Me.PictureBox2.Name = "PictureBox2"
         Me.PictureBox2.Size = New System.Drawing.Size(498, 35)
         Me.PictureBox2.TabIndex = 7
@@ -122,7 +122,7 @@ Partial Class Form1
         '
         'btnLinks
         '
-        Me.btnLinks.Location = New System.Drawing.Point(14, 291)
+        Me.btnLinks.Location = New System.Drawing.Point(14, 238)
         Me.btnLinks.Name = "btnLinks"
         Me.btnLinks.Size = New System.Drawing.Size(105, 23)
         Me.btnLinks.TabIndex = 8
@@ -133,33 +133,31 @@ Partial Class Form1
         '
         Me.txtLinks.BackColor = System.Drawing.SystemColors.Menu
         Me.txtLinks.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.txtLinks.Location = New System.Drawing.Point(138, 291)
+        Me.txtLinks.Location = New System.Drawing.Point(138, 238)
         Me.txtLinks.Name = "txtLinks"
         Me.txtLinks.Size = New System.Drawing.Size(476, 20)
         Me.txtLinks.TabIndex = 9
-        Me.txtLinks.Text = "http://brvix5valedb020.valenet.valeglobal.net:8023/bi_prd/modulos/validate/defaul" &
-    "t_remedy.asp"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(2, 647)
+        Me.Button1.Location = New System.Drawing.Point(410, 523)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(199, 23)
         Me.Button1.TabIndex = 10
         Me.Button1.Text = "Dispara Acesso via Proxy"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'TextBoxProxy
         '
-        Me.TextBox1.Location = New System.Drawing.Point(116, 576)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(127, 20)
-        Me.TextBox1.TabIndex = 11
+        Me.TextBoxProxy.Location = New System.Drawing.Point(410, 473)
+        Me.TextBoxProxy.Name = "TextBoxProxy"
+        Me.TextBoxProxy.Size = New System.Drawing.Size(127, 20)
+        Me.TextBoxProxy.TabIndex = 11
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(113, 559)
+        Me.Label1.Location = New System.Drawing.Point(407, 456)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(33, 13)
         Me.Label1.TabIndex = 12
@@ -168,7 +166,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(115, 606)
+        Me.Label2.Location = New System.Drawing.Point(25, 507)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(29, 13)
         Me.Label2.TabIndex = 14
@@ -176,57 +174,75 @@ Partial Class Form1
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(118, 622)
+        Me.TextBox2.Location = New System.Drawing.Point(28, 523)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(125, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(197, 20)
         Me.TextBox2.TabIndex = 13
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(246, 606)
+        Me.Label3.Location = New System.Drawing.Point(240, 507)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(30, 13)
         Me.Label3.TabIndex = 16
         Me.Label3.Text = "Pass"
         '
-        'TextBox3
+        'TextBoxPass
         '
-        Me.TextBox3.Location = New System.Drawing.Point(249, 622)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(125, 20)
-        Me.TextBox3.TabIndex = 15
+        Me.TextBoxPass.Location = New System.Drawing.Point(243, 523)
+        Me.TextBoxPass.Name = "TextBoxPass"
+        Me.TextBoxPass.Size = New System.Drawing.Size(146, 20)
+        Me.TextBoxPass.TabIndex = 15
         '
-        'TextBox4
+        'TextBoxPorta
         '
-        Me.TextBox4.Location = New System.Drawing.Point(249, 576)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(125, 20)
-        Me.TextBox4.TabIndex = 17
+        Me.TextBoxPorta.Location = New System.Drawing.Point(543, 473)
+        Me.TextBoxPorta.Name = "TextBoxPorta"
+        Me.TextBoxPorta.Size = New System.Drawing.Size(71, 20)
+        Me.TextBoxPorta.TabIndex = 17
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(246, 559)
+        Me.Label4.Location = New System.Drawing.Point(540, 456)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(32, 13)
         Me.Label4.TabIndex = 18
         Me.Label4.Text = "Porta"
+        '
+        'Url
+        '
+        Me.Url.AutoSize = True
+        Me.Url.Location = New System.Drawing.Point(21, 455)
+        Me.Url.Name = "Url"
+        Me.Url.Size = New System.Drawing.Size(20, 13)
+        Me.Url.TabIndex = 20
+        Me.Url.Text = "Url"
+        '
+        'TextBoxUrl
+        '
+        Me.TextBoxUrl.Location = New System.Drawing.Point(24, 473)
+        Me.TextBoxUrl.Name = "TextBoxUrl"
+        Me.TextBoxUrl.Size = New System.Drawing.Size(365, 20)
+        Me.TextBoxUrl.TabIndex = 19
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
-        Me.ClientSize = New System.Drawing.Size(630, 673)
+        Me.ClientSize = New System.Drawing.Size(630, 555)
+        Me.Controls.Add(Me.Url)
+        Me.Controls.Add(Me.TextBoxUrl)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBoxPorta)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBoxPass)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBoxProxy)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.txtLinks)
         Me.Controls.Add(Me.btnLinks)
@@ -239,7 +255,7 @@ Partial Class Form1
         Me.Controls.Add(Me.txtDados)
         Me.Controls.Add(Me.btnObterPagina)
         Me.Name = "Form1"
-        Me.Text = "Usando HttpWebRequest"
+        Me.Text = "HttpRequest"
         CType(Me.dgvDados, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
@@ -258,12 +274,14 @@ Partial Class Form1
     Friend WithEvents btnLinks As System.Windows.Forms.Button
     Friend WithEvents txtLinks As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBoxProxy As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBoxPass As TextBox
+    Friend WithEvents TextBoxPorta As TextBox
     Friend WithEvents Label4 As Label
+    Friend WithEvents Url As Label
+    Friend WithEvents TextBoxUrl As TextBox
 End Class
--- ExtraindoDadosWeb/Form1.vb
+++ ExtraindoDadosWeb/Form1.vb
@@ -17,13 +17,13 @@ Public Class Form1
     Private Sub btnImagem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnImagem1.Click
         Dim strArquivo As String
         Dim wc As New Web
-        strArquivo = wc.RequestImagemWeb("http://brvix5valedb020.valenet.valeglobal.net:8023/bi_prd/modulos/validate/default_remedy.asp", "maco1b.gif")
+        strArquivo = wc.RequestImagemWeb(TextBoxUrl.Text.ToString(), "maco1b.gif")
         PictureBox1.Image = Image.FromFile(strArquivo)
     End Sub
     Private Sub btnImagem2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnImagem2.Click
         Dim strArquivo As String
         Dim wc As New Web
-        strArquivo = wc.RequestImagemWeb("http://brvix5valedb020.valenet.valeglobal.net:8023/bi_prd/modulos/validate/default_remedy.asp", "dvdnet2.gif")
+        strArquivo = wc.RequestImagemWeb(TextBoxUrl.Text.ToString(), "dvdnet2.gif")
         PictureBox2.Image = Image.FromFile(strArquivo)
     End Sub
 
@@ -40,42 +40,26 @@ Public Class Form1
 
 
 
-            Dim request As WebRequest = WebRequest.Create("http://brvix5valedb020.valenet.valeglobal.net:8023/bi_prd/modulos/validate/default_remedy.asp")
-            'http://brvix5valecac003.valenet.valeglobal.net
+            Dim request As WebRequest = WebRequest.Create(TextBoxUrl.Text.ToString())
 
-            Dim wp As WebProxy = New WebProxy(TextBox1.Text.ToString(), Convert.ToInt32(TextBox4.Text))
+            Dim wp As WebProxy = New WebProxy(TextBoxProxy.Text.ToString(), Convert.ToInt32(TextBoxPorta.Text))
 
-            wp.Credentials = New NetworkCredential(TextBox2.Text.ToString(), TextBox3.Text.ToString())
+            wp.Credentials = New NetworkCredential(TextBox2.Text.ToString(), TextBoxPass.Text.ToString())
             request.Proxy = wp
-
-            ' Set the Method property of the request to POST.
             request.Method = "POST"
-            ' Create POST data and convert it to a byte array.
             Dim postData As String = "Isso é um teste de post em um de uma string em um Web server."
             Dim byteArray As Byte() = Encoding.UTF8.GetBytes(postData)
-            ' Set the ContentType property of the WebRequest.
             request.ContentType = "application/x-www-form-urlencoded"
-            ' Set the ContentLength property of the WebRequest.
             request.ContentLength = byteArray.Length
-            ' Get the request stream.
             Dim dataStream As Stream = request.GetRequestStream()
-            ' Write the data to the request stream.
             dataStream.Write(byteArray, 0, byteArray.Length)
-            ' Close the Stream object.
             dataStream.Close()
-            ' Get the response.
             Dim response As WebResponse = request.GetResponse()
-            ' Display the status.
             Console.WriteLine(CType(response, HttpWebResponse).StatusDescription)
-            ' Get the stream containing content returned by the server.
             dataStream = response.GetResponseStream()
-            ' Open the stream using a StreamReader for easy access.
             Dim reader As New StreamReader(dataStream)
-            ' Read the content.
             Dim responseFromServer As String = reader.ReadToEnd()
-            ' Display the content.
             Console.WriteLine(responseFromServer)
-            ' Clean up the streams.
             reader.Close()
             dataStream.Close()
             response.Close()
